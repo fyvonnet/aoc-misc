ce6356dd5226fc8fe7d22c58ff70f98b8e5a4d47
Franck YVONNET <franck@yvonnet.fr>
2020-12-03 08:50:57 +0100

    added function count-valid

--- aoc-misc.lisp
+++ aoc-misc.lisp
@@ -1,7 +1,8 @@
 (defpackage :aoc-misc
   (:use :cl)
   (:export :read-input-as-list
-           :read-input-as-array))
+           :read-input-as-array
+           :count-valid))
 
 (in-package :aoc-misc)
 
@@ -14,10 +15,15 @@
 (defun read-input-as-list (day &optional (func #'identity))
   (with-open-file (stream (format nil "inputs/day~2,'0d" day))
     (read-input stream func)))
-    
+
 
 (defun read-input-as-array (day)
   (let
     ((input-lst (read-input-as-list day)))
     (make-array (list (length input-lst) (length (first input-lst)))
                 :initial-contents input-lst)))
+
+(defun count-valid (predicate lst)
+  (reduce
+    (lambda (c e) (if (funcall predicate e) (1+ c) c))
+    lst :initial-value 0))
